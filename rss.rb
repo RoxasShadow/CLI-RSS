9d11a13ba9e20a5b15122877e5fb1b07d0efdf5b
Giovanni Capuano <webmaster@giovannicapuano.net>
2012-09-03 16:39:51 +0300

    Code rubyfied

--- rss.rb
+++ rss.rb
@@ -1,22 +1,28 @@
+#!/usr/bin/env ruby
 require 'open-uri'
-require 'cgi'
 require 'nokogiri'
 
-abort "Usage:\n #{__FILE__} rss feed\n Example: #{__FILE__} http://www.ruby-lang.org/en/feeds/news.rss" if ARGV.empty?
-
-url = ARGV[0]
-
-doc = Nokogiri::XML(open(url))
-
-titles = doc.css("title")
-i = 0
-
-titles.each do |title|
-	fixed = Nokogiri::HTML(title.to_s).content
-	puts "#{i}: #{fixed}"
-	i += 1
+class String
+	def strip_tags
+		self.gsub(%r{</?[^>]+?>}, '')
+	end
 end
 
-i = Integer(STDIN.gets.chomp)
+abort "Usage:\n ./#{__FILE__} [url feed rss]\n\tExample: #{__FILE__} http://www.ruby-lang.org/en/feeds/news.rss" if ARGV.empty?
 
-puts Nokogiri::HTML(CGI.unescapeHTML(doc.css("description")[i].to_s).to_s).content
+url = ARGV[0]
+i   = 1
+
+doc = Nokogiri::XML(open(url))
+doc.css('title').each { |title|
+	puts "#{i}: #{title.content}"
+	i += 1
+}
+
+print 'Article to read: '
+article = STDIN.gets.chomp.to_i
+puts
+
+abort 'The selected article is not valid.' if article > i
+
+puts doc.css('description')[article - 1].content.strip_tags
